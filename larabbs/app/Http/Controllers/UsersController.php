04095b8c201845463540ec6ae86f2f29137c736d
yiqiao <208274887@qq.com>
2018-04-07 11:44:40 +0800

    用户信息修改权限验证

--- larabbs/app/Http/Controllers/UsersController.php
+++ larabbs/app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
     /**
      * @param \App\Models\User $user
      *
@@ -26,11 +31,13 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
