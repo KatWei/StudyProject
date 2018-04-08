4e373456aabe58877a1a79ead346be0fd2e19793
yiqiao <208274887@qq.com>
2018-04-08 15:00:47 +0800

    Category topic list

--- larabbs/app/Http/Controllers/TopicsController.php
+++ larabbs/app/Http/Controllers/TopicsController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Topic;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 
 class TopicsController extends Controller
