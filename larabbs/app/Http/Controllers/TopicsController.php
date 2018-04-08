d27f3f55270fe0a42e8c443e5a0c9c5caa12a2e2
yiqiao <208274887@qq.com>
2018-04-08 15:14:48 +0800

    Topic order

--- larabbs/app/Http/Controllers/TopicsController.php
+++ larabbs/app/Http/Controllers/TopicsController.php
@@ -4,43 +4,74 @@ namespace App\Http\Controllers;
 
 use App\Models\Topic;
 use App\Http\Requests\TopicRequest;
+use Illuminate\Http\Request;
 
+/**
+ * Class TopicsController
+ *
+ * @package App\Http\Controllers
+ */
 class TopicsController extends Controller
 {
+    /**
+     * TopicsController constructor.
+     */
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
+    public function index(Request $request, Topic $topic)
 	{
-		$topics = Topic::with('user', 'category')->paginate(30);
+		$topics = $topic->withOrder($request->order)->paginate(20);
 		return view('topics.index', compact('topics'));
 	}
 
+    /**
+     * @param \App\Models\Topic $topic
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(Topic $topic)
     {
         return view('topics.show', compact('topic'));
     }
 
-	public function create(Topic $topic)
+    /**
+     * @param \App\Models\Topic $topic
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */public function create(Topic $topic)
 	{
 		return view('topics.create_and_edit', compact('topic'));
 	}
 
-	public function store(TopicRequest $request)
+    /**
+     * @param \App\Http\Requests\TopicRequest $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */public function store(TopicRequest $request)
 	{
 		$topic = Topic::create($request->all());
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
-	public function edit(Topic $topic)
+    /**
+     * @param \App\Models\Topic $topic
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
 		return view('topics.create_and_edit', compact('topic'));
 	}
 
-	public function update(TopicRequest $request, Topic $topic)
+    /**
+     * @param \App\Http\Requests\TopicRequest $request
+     * @param \App\Models\Topic               $topic
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */public function update(TopicRequest $request, Topic $topic)
 	{
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
@@ -48,7 +79,11 @@ class TopicsController extends Controller
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
 	}
 
-	public function destroy(Topic $topic)
+    /**
+     * @param \App\Models\Topic $topic
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */public function destroy(Topic $topic)
 	{
 		$this->authorize('destroy', $topic);
 		$topic->delete();
