4be5919a13eda88ba35ad18cb96752dc2cfcb683
yiqiao <208274887@qq.com>
2018-04-10 13:50:16 +0800

    Add queue for translate

--- larabbs/app/Handlers/SlugTranslateHandler.php
+++ larabbs/app/Handlers/SlugTranslateHandler.php
@@ -7,31 +7,57 @@ use Overtrue\Pinyin\Pinyin;
 
 class SlugTranslateHandler
 {
-    private $api = 'http://api.fanyi.baidu.com/api/trans/vip/translate?';
-    private $app_id;
-    private $key;
-    private $text;
-
-    public function __construct($text, $app_id, $key)
-    {
-        $this->text = $text;
-        $this->app_id = $app_id;
-        $this->key = $key;
-    }
     public function translate($text)
     {
+        // 实例化 HTTP 客户端
+        $http = new Client;
+
+        // 初始化配置信息
+        $api = 'http://api.fanyi.baidu.com/api/trans/vip/translate?';
+        $appid = config('services.baidu_translate.appid');
+        $key = config('services.baidu_translate.key');
+        $salt = time();
 
         // 如果没有配置百度翻译，自动使用兼容的拼音方案
-        if (empty($this->app_id) || empty($this->key)) {
+        if (empty($appid) || empty($key)) {
             return $this->pinyin($text);
         }
 
-        $http = new Client;
+        // 根据文档，生成 sign
+        // http://api.fanyi.baidu.com/api/trans/product/apidoc
+        // appid+q+salt+密钥 的MD5值
+        $sign = md5($appid. $text . $salt . $key);
+
+        // 构建请求参数
+        $query = http_build_query([
+            "q"     =>  $text,
+            "from"  => "zh",
+            "to"    => "en",
+            "appid" => $appid,
+            "salt"  => $salt,
+            "sign"  => $sign,
+        ]);
 
         // 发送 HTTP Get 请求
-        $response = $http->get($this->str_query());
+        $response = $http->get($api.$query);
 
         $result = json_decode($response->getBody(), true);
+
+        /**
+        获取结果，如果请求成功，dd($result) 结果如下：
+
+        array:3 [▼
+        "from" => "zh"
+        "to" => "en"
+        "trans_result" => array:1 [▼
+        0 => array:2 [▼
+        "src" => "XSS 安全漏洞"
+        "dst" => "XSS security vulnerability"
+        ]
+        ]
+        ]
+
+         **/
 
         // 尝试获取获取翻译结果
         if (isset($result['trans_result'][0]['dst'])) {
@@ -42,25 +68,8 @@ class SlugTranslateHandler
         }
     }
 
-    private function pinyin($text)
+    public function pinyin($text)
     {
         return str_slug(app(Pinyin::class)->permalink($text));
-    }
-
-    private function str_query()
-    {
-        $salt = time();
-        $sign = md5($this->app_id . $this->text . $salt . $this->key);
-        // 构建请求参数
-        $query = http_build_query([
-            "q"     =>  $this->text,
-            "from"  => "zh",
-            "to"    => "en",
-            "appid" => $this->app_id,
-            "salt"  => $salt,
-            "sign"  => $sign,
-        ]);
-
-        return $this->api . $query;
     }
 }
