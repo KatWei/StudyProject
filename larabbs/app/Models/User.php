5bc2c7fdddcc24e800df53d601871b719b338c43
yiqiao <208274887@qq.com>
2018-04-11 14:33:14 +0800

    Admin can manage users

--- larabbs/app/Models/User.php
+++ larabbs/app/Models/User.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
 
+/**
+ * Class User
+ *
+ * @package App\Models
+ */
 class User extends Authenticatable
 {
     use HasRoles;
@@ -14,6 +19,9 @@ class User extends Authenticatable
      notify as protected laravelNotify;
     }
 
+    /**
+     * @param $instance
+     */
     public function notify($instance)
     {
         if ($this->id === Auth::id()) {
@@ -41,19 +49,50 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function topics()
     {
         return $this->hasMany(Topic::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function replies()
     {
         return $this->hasMany(Reply::class);
     }
 
+    /**
+     * @param $model
+     *
+     * @return bool
+     */
     public function isAuthorOf($model)
     {
         return $this->id == $model->user_id;
+    }
+
+    /**
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
+    {
+        if (strlen($value) != 60) {
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        if (!starts_with($path, 'http')) {
+            $path = config('app.url').'/uploads/images/avatar/$path';
+        }
+        $this->attributes['avatar'] = $path;
     }
 
     public function markAsRead()
